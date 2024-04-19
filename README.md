de35f79ccaa5a860d3b085eee056dcb7f0fa1309
Alex Pogosyan <git@alexpogosyan.com>
2024-04-19 14:03:29 +0400

    Use status bar instead of info popup

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Emacs-style set mark and select region feature in VS Code.
 
 ## Features
 
-- Set start/end mark with Toggle mark command
+- Set start/end mark with Toggle mark command (Cmd-Alt-K by default)
 
 - Select arbitrary region
--- src/extension.ts
+++ src/extension.ts
@@ -2,29 +2,36 @@ import * as vscode from "vscode";
 
 let mark: vscode.Position | null = null;
 let isSelecting = false;
+let statusBarItem: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
+  statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
+  context.subscriptions.push(statusBarItem);
+
   const toggleMarkCommand = vscode.commands.registerCommand(
     "select-region.toggleMark",
     () => {
       const editor = vscode.window.activeTextEditor;
       if (!editor) return;
 
-      if (!isSelecting) {
-        mark = editor.selection.active;
-        isSelecting = true;
-        vscode.window.showInformationMessage("Start mark set");
-      } else {
+      if (isSelecting) {
         if (mark) {
           const currentPos = editor.selection.active;
           editor.selection = new vscode.Selection(mark, currentPos);
-          vscode.window.showInformationMessage("Region selected");
         }
         isSelecting = false;
+        statusBarItem.hide();
+      } else {
+        mark = editor.selection.active;
+        isSelecting = true;
+        statusBarItem.text = "Start mark set";
+        statusBarItem.show();
       }
     }
   );
-
   context.subscriptions.push(toggleMarkCommand);
 }
 
